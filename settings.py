12c5445b553ba5274450c3582a3e8fffac4ef8d3
Heuyie <junkmailsfortwo@gmail.com>
2017-10-29 13:02:51 -0700

    Initial commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+#These are examples.
+beforeEach = "email = undefined;\n    console.log('new test');"
+afterEach = "console.log('A test is over!');"
+
+testClass = 'Email'
+
+variableName = 'email'
+
+included = ['number', 'good email']
+accepted = ['good email']
+functionHeaderDict = {'all':'validateEmail(email)'}
+toEqualDict = {'accepted':'true', 'rejected':'false'}
